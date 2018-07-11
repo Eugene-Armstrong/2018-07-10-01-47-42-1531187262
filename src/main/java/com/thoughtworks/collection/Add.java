bf4a374cf2a37a9623a11c0ec7cb5e4beca856e8
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-11 20:56:20 +0800

    [Jeffery] Passed most of Add.java

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,27 +2,84 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int sum = 0;
+        int chk = leftBorder - rightBorder;
+        int theLeft, theRight;
+        if(chk<0){
+            theLeft = leftBorder;
+            theRight = rightBorder;
+        }else{
+            theLeft = rightBorder;
+            theRight = leftBorder;
+        }
+        for(int i=theLeft; i<=theRight; i++){
+            if(i%2==0){
+                sum += i;
+            }
+        }
+        return sum;
+//        throw new NotImplementedException();
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int sum = 0;
+        int chk = leftBorder - rightBorder;
+        int theLeft, theRight;
+        if(chk<0){
+            theLeft = leftBorder;
+            theRight = rightBorder;
+        }else{
+            theLeft = rightBorder;
+            theRight = leftBorder;
+        }
+        for(int i=theLeft; i<=theRight; i++){
+            if(i%2!=0){
+                sum += i;
+            }
+        }
+        return sum;
+//        throw new NotImplementedException();
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> sums = arrayList.stream().
+                map(n -> 3 * n + 2).
+                collect(Collectors.toList());
+        int result = 0;
+        for(int s :sums){
+            result += s;
+        }
+        return result;
+//        throw new NotImplementedException();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> sums = arrayList.stream().
+                map(n -> n%2!=0?3*n+2:n).
+                collect(Collectors.toList());
+        return sums;
+//        throw new NotImplementedException();
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> sums = arrayList.stream().
+                map(n -> n%2!=0?3*n+5:0).
+                collect(Collectors.toList());
+        int result = 0;
+        for(int s :sums){
+            result += s;
+        }
+        return result;
+//        throw new NotImplementedException();
     }
 
     public double getMedianOfEven(List<Integer> arrayList) {
@@ -30,22 +87,58 @@ public class Add {
     }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        double sum = 0, count = 0;
+        for(int al : arrayList){
+            if(al%2==0){
+                sum += al;
+                count++;
+            }
+        }
+        return sum/count;
+//        throw new NotImplementedException();
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        throw new NotImplementedException();
+        Integer[] evens = arrayList.stream().
+                filter(n -> n%2 == 0).toArray(Integer[]::new);
+        return Stream.of(evens).anyMatch(n -> n == specialElment);
+//        throw new NotImplementedException();
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> tmp = arrayList.stream().
+                distinct().collect(Collectors.toList());
+
+        Integer[] evens = tmp.stream().
+                filter(n -> n%2 == 0).toArray(Integer[]::new);
+
+        return Arrays.asList(evens);
+//        throw new NotImplementedException();
     }
 
     public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        Integer[] evens = arrayList.stream().
+                filter(n -> n%2 == 0).toArray(Integer[]::new);
+        Integer[] odds = arrayList.stream().
+                filter(n -> n%2 != 0).toArray(Integer[]::new);
+        Arrays.sort(evens);
+        Arrays.sort(odds);
+        Collections.reverse(Arrays.asList(odds));
+        List<Integer> resultList = Arrays.asList(evens);
+        for(int o : odds){
+            resultList.add(o);
+        }
+        return resultList;
+//        throw new NotImplementedException();
     }
 
     public List<Integer> getProcessedList(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        Integer[] result = new Integer[arrayList.size()-1];
+        for(int i=0;i<arrayList.size()-1;i++){
+            result[i] = 3*(arrayList.get(i)+arrayList.get(i+1));
+        }
+        List<Integer> resultList = Arrays.asList(result);
+        return resultList;
+//        throw new NotImplementedException();
     }
 }
--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,29 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        int chk = left - right;
+        int theLeft, theRight;
+        Integer[] result;
+        if(chk<0){
+            result = new Integer[right-left+1];
+            for(int i = 0; i<= right-left;i++){
+                result[i] = left+i;
+            }
+        }else{
+            result = new Integer[left-right+1];
+            for(int i = 0; i<= right-left;i++){
+                result[i] = left-i;
+            }
+        }
+        List<Integer> resultList = Arrays.asList(result);
+        return resultList;
+//        throw new NotImplementedException();
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
